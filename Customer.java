5ad58c82f14c330d75e79e3dbb1df84e3471f947
john ralph <johnralphramirez@gmail.com>
2026-02-03 12:56:02 +0800

    blank

--- Customer.java
+++ Customer.java
@@ -1,26 +1,2 @@
-package CC3Correctdawto;
 
-// Member 2's unique class
-public class Customer {
-    private String nameOfCustomer; // Member variable [cite: 12]
-    private double money;
-
-    public Customer(String nameOfCustomer, double money) {
-        this.nameOfCustomer = nameOfCustomer;
-        this.money = money;
-    }
-
-    // Functional method [cite: 13]
-    public String processPayment(double amount) {
-        if (money >= amount) {
-            return "Payment successful for " + nameOfCustomer;
-        }
-        return "Insufficient funds for " + nameOfCustomer;
-    }
-
-    // Identity method [cite: 18]
-    public String identifyRole() {
-        return "I am the Customer Class. I manage the buyer's identity and their available funds. [cite: 21]";
-    }
-}
 
