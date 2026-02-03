236a63cd101ac03c5b8a8d81b7db22c2589ef674
john ralph <johnralphramirez@gmail.com>
2026-02-03 12:46:36 +0800

    ShoppingCart2

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -14,3 +14,18 @@ public class ShoppingCart {
     public void addProduct(Product p) {
         products.add(p);
     }
+    
+    // Functional method [cite: 13]
+    public double calculateTotal() {
+        double total = 0;
+        for (Product p : products) {
+            total += p.getPrice();
+        }
+        return total;
+    }
+
+    // Identity method [cite: 18]
+    public String identifyRole() {
+        return "I am the ShoppingCart Class. I organize products and link them to a specific customer. [cite: 21]";
+    }
+}
